624a3585ba6dd929ab410ebcd78dd3a75adf4048
Maciej Kociemski <maciejkociemski@MBP-Maciej.lan>
2024-02-06 13:53:24 +0100

    aktualizskondensowano umiejetnosci na potrzeby uniwersalnosci cv dla pracuj.pl

--- cv_universal_pl/readme.pl
+++ cv_universal_pl/readme.pl
@@ -1,3 +1,5 @@
-cv w jezyku polskim jako uniwersalne , aktualizacja 2024-02-06 g.12.01
+cv w jezyku polskim jako uniwersalne , aktualizacja 2024-02-06 g.14.01
 
 dodano info o prawie jazdy
+
+skondensowano umiejetnosci na potrzeby uniwersalnosci cv dla pracuj.pl
