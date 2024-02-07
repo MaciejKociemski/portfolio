d5055e83b27ac4bcbe23ae04d886dd3b4e97d20f
Maciej Kociemski <maciejkociemski@MBP-Maciej.lan>
2024-02-07 10:44:13 +0100

    add skill

--- cv_universal_pl/readme.pl
+++ cv_universal_pl/readme.pl
@@ -3,3 +3,6 @@ cv w jezyku polskim jako uniwersalne , aktualizacja 2024-02-06 g.14.01
 dodano info o prawie jazdy
 
 skondensowano umiejetnosci na potrzeby uniwersalnosci cv dla pracuj.pl
+
+2024-02-07 10:43
+dodano skill analiza 
